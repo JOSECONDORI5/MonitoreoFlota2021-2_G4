c0fcc15312be767c59b32737875d69fbe598d7b2
diegosilva96 <92829873+diegosilva96@users.noreply.github.com>
2022-01-04 12:41:42 -0500

    Pregunta 1

--- src/main/java/monitoreo/modelos/impl/MotoStrategy.java
+++ src/main/java/monitoreo/modelos/impl/MotoStrategy.java
@@ -4,9 +4,15 @@ import monitoreo.modelos.interfaces.ITransporteStrategy;
 
 public class MotoStrategy implements ITransporteStrategy {
 
+    
+    private int MaxPuntos=8;
+    private double MaxPeso=50;
+    private Double[][] puntosEntregaVariado;
+    private double peso=0;
+    
     public Double[][] crearPuntos(Double[][] puntos) {
 
-        Double[][] puntosEntregaVariado = {
+        puntosEntregaVariado = new  Double[][]{
                 {-12.054456, -77.083491},
                 {-12.056025, -77.081278},
                 {-12.056520, -77.080370},
@@ -18,5 +24,19 @@ public class MotoStrategy implements ITransporteStrategy {
                 {-12.059249, -77.075532}
         };
         return puntosEntregaVariado;
+    }
+    public void AgregarPuntos(Double[][] puntos){
+        if(MaxPuntos-puntosEntregaVariado.length>1){
+            puntosEntregaVariado[puntosEntregaVariado.length+1][0]=puntos[0][0];
+            puntosEntregaVariado[puntosEntregaVariado.length+1][1]=puntos[0][1];
+            if(peso+puntos[0][2]<MaxPeso){
+                System.out.println("Peso excedido para la moto");
+            }else{
+                peso+=puntos[0][2];
+            }
+        }else{
+            System.out.println("Se alcanzó el límite de puntos");
+        }
+      
     }
 }
